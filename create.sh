da0a6a632325d84b76ebb93e570d575c7b1c0fe7
Wisanu Aon-In <108912837+JOTARO365@users.noreply.github.com>
2024-01-07 17:07:13 +0700

    Add files via upload

---
+++ create.sh
@@ -0,0 +1,30 @@
+date_new=$(date)
+echo "============================================="
+echo "=                                           ="
+echo "= start create folder and cut file today :D ="
+echo "=      $date_new         ="
+echo "=                                           ="
+echo "============================================="
+
+
+while true; do
+	
+	current_date=$(date +%m%d%y)
+	mk_folder="/mnt/c/Users/User/Desktop/scan/$current_date"
+	file_to_move="mnt/c/Users/User/Desktop/scan/.pdf"
+
+	if [ ! -d "$mk_folder" ]; then
+		mkdir -p "$mk_folder" 
+		echo "Created folder : $mk_folder"
+	else
+		echo " :D Create folder $mk_folder is already existed"
+	fi
+	if ls $file_to_move 1> /dev/null 2>&1; then
+		mv $file_to_move "$mk_folder/"
+		echo "Moved files to : $mk_folder"
+	else
+		echo " move file not found :("
+	fi
+
+	sleep 3
+done
